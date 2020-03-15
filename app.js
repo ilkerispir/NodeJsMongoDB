3806b4097841ff62c695e73a1c5fe816cf0b012f
ilkerispir <ikerispir@gmail.com>
2020-03-16 01:08:13 +0300

    morgan

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRouters = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+//Routes which should handle requests 
 app.use('/products', productRouters);
 app.use('/orders', orderRouters);
+
+app.use((res, req, next) =>{
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error : {
+            message : error.message
+        }
+    });
+});
 
 module.exports = app;
