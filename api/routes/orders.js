2d5844fabbb587a22d7096eba6fa30509390c642
ilkerispir <ikerispir@gmail.com>
2020-03-16 01:49:08 +0300

    body-parser

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,8 +9,13 @@ router.get('/', (req, res, next) =>{
 });
 
 router.post('/', (req, res, next) =>{
+    const order = {
+        orderId : req.body.orderId,
+        quantity : req.body.quantity
+    };
     res.status(201).json({
-        message : 'Order was created'
+        message : 'Order was created',
+        order : order
     });
 });
 
