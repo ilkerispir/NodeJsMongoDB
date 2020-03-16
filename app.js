ea72a41397eb9d6791555f8fd68e836c82c25863
ilkerispir <ikerispir@gmail.com>
2020-03-16 04:28:25 +0300

    Proucts CRUD

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRouters = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
+
+mongoose.connect(
+    'mongodb+srv://ilker:'+ 
+    process.env.MONGO_ATLAS_PW +
+    '@mflix-vxo3s.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
