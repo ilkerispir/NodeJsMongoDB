8d8c4d1f9f3574cc7aa363e49701c364ec5f9dca
ilkerispir <ikerispir@gmail.com>
2020-03-23 01:24:54 +0300

    order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -10,6 +10,7 @@ router.get('/', (req, res, next) =>{
     Order
     .find()
     .select('product quantity _id')
+    .populate('product', 'name')
     .exec()
     .then(docs => {
         console.log(order);
@@ -76,7 +77,9 @@ router.post('/', (req, res, next) =>{
 });
 
 router.get('/:orderId', (req, res, next) => {
-    Order.findById(req.params.orderId)
+    Order
+    .findById(req.params.orderId)
+    .populate('product')
     .exec()
     .then(order =>{ 
         if(!order){
